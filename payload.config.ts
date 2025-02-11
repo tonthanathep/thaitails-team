fd9a325663a6adbd839268c41ffb5b8b5ba1bf01
Thanathep Thaithae <justminemind@gmail.com>
2025-02-11 18:56:03 +0700

    initial commit

---
+++ payload.config.ts
@@ -0,0 +1,29 @@
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import { buildConfig } from "payload";
+import sharp from "sharp";
+import { Users } from "./collection/Users";
+
+export default buildConfig({
+  // If you'd like to use Rich Text, pass your editor here
+  editor: lexicalEditor(),
+  routes: {
+    admin: "/tubtim",
+  },
+
+  // Define and configure your collections in this array
+  collections: [Users],
+
+  // Your Payload secret - should be a complex and secure string, unguessable
+  secret: process.env.PAYLOAD_SECRET || "",
+  // Whichever Database Adapter you're using should go here
+  // Mongoose is shown as an example, but you can also use Postgres
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI || "",
+  }),
+  // If you want to resize images, crop, set focal point, etc.
+  // make sure to install it and pass it to the config.
+  // This is optional - if you don't need to do these things,
+  // you don't need it!
+  sharp,
+});
