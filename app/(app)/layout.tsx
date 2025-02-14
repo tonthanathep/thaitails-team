40ddd357a21f038f3e381749dcf815d466d6abe5
Thanathep Thaithae <justminemind@gmail.com>
2025-02-14 19:07:57 +0700

    fix unused var

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,18 +1,11 @@
 import type { Metadata } from "next";
-import { Noto_Sans_Thai, Nunito_Sans } from "next/font/google";
+import { Noto_Sans_Thai } from "next/font/google";
 import "./globals.css";
 
 const notoSansThai = Noto_Sans_Thai({
   variable: "--font-noto-sans-thai",
   subsets: ["thai", "latin"],
   weight: "variable",
-});
-
-const nunitoSans = Nunito_Sans({
-  variable: "--font-nunito-sans",
-  subsets: ["latin"],
-  weight: "variable",
-  style: ["normal"],
 });
 
 export const metadata: Metadata = {
